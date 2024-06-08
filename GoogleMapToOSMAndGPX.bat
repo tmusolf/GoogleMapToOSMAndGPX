6bc5b005b6a65f0516d2c178b303c97304043118
tmusolf <34257180+tmusolf@users.noreply.github.com>
2024-06-08 01:18:04 -0700

    Updated comments & allow individual parms to be specified for each GMap processed
    Added another way to specify the command line parms for the utility.  You can now specify them for each individual GMap listed in the input file.
    
    Updated comments

--- GoogleMapToOSMAndGPX.bat
+++ GoogleMapToOSMAndGPX.bat
@@ -1,19 +1,29 @@
- @echo off
+@echo off
 echo.
 echo Converts google my maps into OSMAnd GPX files.
-:: Reads in a supplied file containing a google map name and its corresponding ID value (mid=xxxx)
-:: The google map name does not have to be the actual name, it is the string you want used for the folder.
-::
-:: If no parameters are supplied batch file will ask for filename and use fixed parameters
-:: You can also invoke the batch file with 2 parameters
-::     GoogleMapToOSMAndGPX <filename> <parms>
-:: The optional 2nd parameter is a string of program parms to replace default ones - used for testing
+echo.
+echo Reads in a supplied input file containing lines of text.
+echo Each line represents one GMap that you want translated.
+echo Each line has the following format.  Three tokens separated by commas:
+echo   (Directory Path),(MapID),(parms)
+echo   (Directory Path): Required, the path\directory name where you want the resulting GPX files
+echo   (MapID): Required, the GMap ID obatined from the map URL. It's the string of characters between "mid=" and the first "&" character after that.
+echo   (parms): Options command line parms for the GoogleMapToOSMAndGPX utility
+echo.
+echo   For example:
+echo     F:\TestMaps\LayersTracksWayPts,ZZZZXXXXZZZZ_xxxxxxxxxxxx, -t 80 -l
+echo.
+echo If no parameters are supplied batch file will ask for filename and use fixed parameters
+echo You can also invoke the batch file with 2 parameters
+echo     GoogleMapToOSMAndGPX.bat "(filename)" "(parms)"
+echo The optional 2nd parameter (parms) is a string of GoogleMapToOSMAndGPX parms to replace default ones.  Double quotes required.
+echo.
 setlocal EnableDelayedExpansion
 set PROGRAM="U:\Projects\Computer Projects\PC Software\GoogleMapToOSMAndGPX\GoogleMapToOSMAndGPX.py"
 :: first check if the input file is specified on the command line
 if "%~1" == "" (
 	echo.
-	set /p infile= "  Enter path to file with comma separated pairs <name>,<mapID> values: "
+	set /p infile= "  Enter path to input file: "
 ) else (
 	set infile="%~1"
 )
@@ -35,21 +45,27 @@ set /a count=0
 set /a errorCount=0
 
 :: Read the file containing infile and outfile names
-:: Tokens split on comma, first token put in %%A and second in %%B
+:: Tokens split on comma, first token put in %%A and second in %%B, optional 3rd is %%C
 :: Lots of batch file crap having to do with file names with spaces
 :: and how they are quoted.  On input from cmd line use double quotes
 :: around paths that have spaces.  Single quotes won't work.  Here in the batch
 :: file I found a reference that said use single quotes and type command
 :: to get around these issues.
-for /f "eol=# tokens=1,2 delims=," %%A in ('type %infile%') do (
+for /f "eol=# tokens=1,2,3 delims=," %%A in ('type %infile%') do (
 	set /a count+=1
 	set "outputfolder=%%A-%CurrentDate%"
 	echo =========================================================================================
 	echo Processing map #!count!
-	if "%~2" == "" (
-		py %PROGRAM% %%B "!outputfolder!" -t 80 -w 12
-	) else (
+	if "%~2" NEQ "" (
+		:: Command line specified parms overide defaults and those specified in input file
+		echo %~2
 		py %PROGRAM% %%B "!outputfolder!" %~2
+	) else if "%%C" NEQ "" (
+		:: If there is a third comma separated token from input file use it to override default parameters
+		py %PROGRAM% %%B "!outputfolder!" %%C
+	) else (
+		:: Use the default parameters
+		py %PROGRAM% %%B "!outputfolder!" -t 80 -w 12
 	)
 	if !ERRORLEVEL! NEQ 0 (
 		set /a errorCount+=1
